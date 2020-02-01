f04be73c351dd12e37c3a0d47c870d9394f3d511
YongquanYao <rrraymond810@gmail.com>
2020-02-01 15:51:58 -0500

    autoTyoe slogen

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,12 +3,13 @@
     <div class="home">
             <div class="home-title"> 
                 <h1 class="title">{{title}}</h1>
-                <div>
+                <!-- <div>
                 <span>{{slogen1}}</span>
                 </div>
                 <div>
                 <span>{{slogen2}}</span>
-                </div>
+                </div> -->
+                <div class="show" v-html="fContent"></div>
                 <div>
                 <router-link to="/home/info">
                      <a-button @click="openNotification">{{startButton}}<a-icon type="arrow-right"></a-icon></a-button>
@@ -21,18 +22,111 @@
     </div>
 </template>
 
+
 <script>
     export default {
+        props: {
+
+        writeTime: {
+
+            type:Number,
+
+            default: 100
+
+        },
+
+        content:{
+            type:Object,
+            default: function () {
+                return {
+                    str1:'Your best partner,',
+                    str2:'the most powerful system in the world.',
+                }
+            }
+        }
+    },
         data(){
             return{
                 title: 'Merchant System',
-                slogen1: 'Your best partner,',
-                slogen2: 'the most powerful system in the world.',
-                startButton: 'Get Started'
+                // slogen1: 'Your best partner,',
+                // slogen2: 'the most powerful system in the world.',
+                startButton: 'Get Started',
+
+                fContent:"",
+                inContent:"",
+                mContent:"",
+                course:"|",
+                hasCourse:true
             }
         },
-         methods: {
-      openNotification() {
+        //页面加载时
+         mounted(){
+            this.showContent();
+            this.courseTimeOut();
+        },
+        methods: {
+          showContent(){
+            var that = this;
+            var data = that.content;
+            var mData = "";
+            for (var i in data){
+                mData += data[i] + '`';
+            }
+            that.mContent = mData;
+            that.contentTimeOut(0);
+        },
+          handStr(j){
+            var that = this;
+            var count = j;
+            var str = that.mContent;
+            count++;
+            if(count >= str.length){
+                that.hasCourse = false;
+                that.fContent = that.inContent;
+            }else{
+                if(" " == str.charAt(j)){
+                    that.inContent += '&ensp;';
+                }else if("`" == str.charAt(j)){
+                    that.inContent += "</br>";
+                }else{
+                    that.inContent += str.charAt(j);
+                }
+                that.fContent = that.inContent + that.course;
+                that.contentTimeOut(count);
+            }
+        },
+        contentTimeOut(value){
+
+            var that = this;
+
+            setTimeout(function () {
+
+                that.handStr(value)
+
+            },that.writeTime)
+
+        },
+        //输入符号的速度
+        courseTimeOut(){
+            var that = this
+            setTimeout(function () {
+                if(that.course.length > 0){
+                    that.fContent = that.inContent + "";
+                    that.course = "";
+                    that.courseTimeOut();
+                }else{
+                    if(that.hasCourse){
+                        that.fContent = that.inContent + "|";
+                        that.course = "|";
+                        that.courseTimeOut();
+                    }else{
+                        that.fContent = that.inContent;
+                    }
+                }
+            },200)
+        },
+        // Notification pop up when enter the homepage
+        openNotification() {
         this.$notification.open({
           message: 'Introduction Notification',
           description:
@@ -67,7 +161,7 @@
     transition: 1s;
     cursor: pointer;
 }
-.home-title span{
+.home-title .show{
     font-size:25px;
     font-weight: 400;
     color:rgba(17,17,17,0.4);
@@ -99,65 +193,6 @@ button{
   100%{
     transform: scale(0.8);
   }
-}
-
-#wrapper{
-  margin:100px auto;
-  width:130px;
-  height:50px;
-  background: #fff;
-  border-radius:6px;
-  color: #000;
-  text-align:center;
-  line-height:50px;
-  font-size:15px;
-  transition: 2s;
-  position: absolute;;
-  cursor:pointer;
-  right:0;
-  bottom:-10%;
-}
-#wrapper:hover{
-  color: #000;
-  background: #fff;
-
-}
-
-#top, #bottom{
-    height:3px;
-    width: 0px;
-    background:#fff;
-    position:absolute;
-    border-radius:1px;
-    transition: 2s 0.5s;
-}
-#top, #left{
-  top: 0;
-  left:0;
-}
-#bottom, #right{
-  bottom: 0;
-  right:0;
-}
-
-#wrapper:hover #top, #wrapper:hover #bottom{
-  width:60px;
-  background: #000;
-
-}
-
-#right, #left{
-  width:3px;
-  height:0px;
-  background:#fff;
-  position:absolute;
-  border-radius:4px;
-  transition: 2s 0.5s;
-}
-
-#wrapper:hover #left, #wrapper:hover #right{
-  height: 20px;
-  background:#000;
 }
 
 </style>
