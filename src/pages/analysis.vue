99423a71b9168b46c7c4a18945e9aa349d412bb7
Raymond <rrraymond810@gmail.com>
2020-09-19 19:45:18 -0400

    update analysis automation

--- src/pages/analysis.vue
+++ src/pages/analysis.vue
@@ -3,11 +3,9 @@
         <a-row type="flex" justify="space-between">
              <a-card class="line-chart">
                 <line-chart :chart-data="linedata1"></line-chart>
-            <!-- <a-button @click="fillData()">Randomize</a-button> -->
             </a-card>
             <a-card class="line-chart">
                 <line-chart :chart-data="linedata2"></line-chart>
-            <!-- <a-button @click="fillData()">Randomize</a-button> -->
             </a-card>
             <a-card class="bar-chart">
                 <bar-chart :chart-data="bardata"></bar-chart>
@@ -17,8 +15,18 @@
             </a-card>
         </a-row>
          <a-row type="flex" justify="space-between">
-            <a-card class="bubble-chart">
-                <pie-chart :chart-data="linedata1"></pie-chart>
+            <a-card class="bottom-chart">
+                <pie-chart :chart-data="piedata"></pie-chart>
+            </a-card>
+            <a-card class="middle-card">
+                <a-button ref="play" class="radmonBtn" @click="fillData()">Randomize</a-button>
+                <a-button class="radmonBtn" @click="cancelChartplay()">Stop</a-button>
+                <a-table :columns="columns" :data-source="tabledata" :pagination='false'>
+                    <!-- <a slot="name" slot-scope="text">{{ text }}</a> -->
+                </a-table>
+            </a-card>
+            <a-card class="bottom-chart">
+                <radar-chart :chart-data="radardata"></radar-chart>
             </a-card>
          </a-row>
             <!-- Bar,
@@ -38,23 +46,60 @@
     import BarChart from '../components/Chart/BarChart.js'
     import BubbleChart from '../components/Chart/BubbleChart.js'
     import PieChart from '../components/Chart/PieChart.js'
+    import RadarChart from '../components/Chart/RadarChart.js'
     // import { Line } from 'vue-chartjs'
     export default {
+        props:{
+            chartData: {}
+        },
         components:{
             LineChart,
             BarChart,
             BubbleChart,
-            PieChart
+            PieChart,
+            RadarChart
         },
         data () {
             return {
-                linedata: null,
+                linedata1: null,
+                linedata2: null,
                 bardata: null,
                 bubbledata: null,
+                piedata: null,
+                radardata: null,
+                
+                columns:[
+                    {
+                        title: 'One',
+                        dataIndex: 'one',
+                        key: 'one',
+                        scopedSlots: { customRender: 'one' },
+                        sorter: true,
+                    },
+                    {
+                        title: 'Two',
+                        dataIndex: 'two',
+                        key: 'two',
+                        scopedSlots: { customRender: 'two' },
+                        sorter: true,
+                    },
+                    {
+                        title: 'Three',
+                        dataIndex: 'three',
+                        key: 'three',
+                        scopedSlots: { customRender: 'three' },
+                        sorter: true,
+                    },
+                ],
+                tabledata: null,
+                interval: setInterval(() => {
+                    this.fillData()
+                },2000)
             }
         },
         mounted () {
             this.fillData()
+            this.chartPlay()
         },
         methods: {
             fillData () {
@@ -146,9 +191,48 @@
                         }
                     ]
                 }
+                this.piedata = {
+                    labels: ['One', 'Two', 'Three', 'Four', 'Five', 'Six'],
+                     datasets: [
+                        {
+                        backgroundColor: ['#f88080', '#ebebeb', '#eb4034','#999','#db1709','#555'],
+                        data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+                        }
+                    ]
+                },
+                this.radardata = {
+                    labels: ['Januaray', 'February', 'March', 'April', 'May', 'June'],
+                     datasets: [
+                        {
+                        label: 'Radar',
+                        backgroundColor: ['#f88080', '#ebebeb', '#eb4034','#999','#db1709','#555'],
+                        data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+                        }
+                    ]
+                },
+                this.tabledata = [
+                    {
+                        key: 'one',
+                        one: this.getRandomInt(),
+                        two: this.getRandomInt(),
+                        three: this.getRandomInt(),
+                    },
+                    {
+                        key: 'two',
+                        one: this.getRandomInt(),
+                        two: this.getRandomInt(),
+                        three: this.getRandomInt(),
+                    },
+                ]
             },
             getRandomInt () {
                 return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+            },
+            chartPlay(){
+                this.interval
+            },
+            cancelChartplay(){
+                clearInterval(this.interval);
             }
         }
     }
@@ -166,5 +250,19 @@
     .bubble-chart{
         width: 285px;
         margin-top: 40px;
+    }
+    .bottom-chart{
+        width: 285px;
+        margin-top: 25px;
+    }
+    .middle-card{
+        width: 500px;
+        height: 260px;
+        margin-top: 25px;
+    }
+    .radmonBtn{
+        margin-bottom: 3px;
+        font-size: 13px;
+        margin-right: 4px;
     }
 </style>
